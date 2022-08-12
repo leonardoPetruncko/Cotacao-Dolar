684e6b69f1ee6de6edc24ea44f74ddf96d2c92ae
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-08-11 22:46:56 -0300

    0.3 JS Date local

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function text(x) {
 let submit = document.getElementById("submit")
 let cotacao = document.getElementById("cotacao")
 
+submit.addEventListener("click", () => {
+    let dia1 = new Date(document.getElementById("diaAtual").value)
+    let dia2 = new Date(document.getElementById("diaFinal").value)
+    console.log(dia1.toLocaleString(), dia2.toLocaleString())
+})
