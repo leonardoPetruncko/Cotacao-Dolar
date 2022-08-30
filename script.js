6b454b1feaf729f3857f94d864dfa59d0f512ea5
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-08-30 01:11:42 -0300

    TESTE API 0.2

--- script.js
+++ script.js
@@ -5,21 +5,46 @@ function text(x) {
 }
 
 let submit = document.getElementById("submit")
-let cotacao = document.getElementById("cotacao")
+let cotacaoCompra = document.getElementById("cotacao")
+let cotacaoVenda = document.getElementById("cotacao")
+let cotacaoTotal = 0
+let cotacaoMedia = 0
+let qtdItens = 0
 
-function getResult ( dataCotacao ){
-    return fetch ('https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='`${dataCotacao}`'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda').then(response => response.json())    
+function getSingleResult(dataCotacao) {
+    return fetch(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${dataCotacao}'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda`)
+        .then(response => response.json())
+        .then((response) => {
+            cotacaoCompra = response.value[0].cotacaoCompra;
+            cotacaoVenda = response.value[0].cotacaoVenda;
+            console.log('cotacaoVenda', cotacaoVenda)
+            return console.log('cotacaoCompra', cotacaoCompra)
+        })
 }
 
-submit.addEventListener("click", () => {
+function getPeriodResult(dataInicial, dataFinal) {
+    return fetch(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='${dataInicial}'&@dataFinalCotacao='${dataFinal}'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda`)
+        .then(response => response.json())
+        .then((values) => {
+            return values.value.map(value => {
+                console.log('ta')
+                qtdItens++;
+                cotacaoTotal = cotacaoTotal + value.cotacaoVenda
+                cotacaoMedia = cotacaoTotal / qtdItens
+            })
+        })
+}
 
+submit.addEventListener("click", async () => {
+    let dia1 = (document.getElementById("diaAtual").value)
+    let dia2 = (document.getElementById("diaFinal").value)
 
-    let dia1 =(document.getElementById("diaAtual").value)
-    let dia2 =(document.getElementById("diaFinal").value)
+    const dataInicialFormatada = new Date(dia1).toLocaleDateString("en-US");
+    const dataFinalFormatada = new Date(dia2).toLocaleDateString("en-US");
 
-    const date = new Date(dia2).toLocaleDateString("en-US");
-    getResult(date)
-    
-    console.log(date)
+    if (!dia1) return getSingleResult(dataFinalFormatada)
+    await getPeriodResult(dataInicialFormatada, dataFinalFormatada)
 
+    console.log('media', cotacaoMedia)
+    console.log('itens', qtdItens)
 })
