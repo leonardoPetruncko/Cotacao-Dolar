a1db544a92a1f79f0a1dab6c6a236013b4644697
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-08-29 23:49:24 -0300

    teste

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ function text(x) {
 let submit = document.getElementById("submit")
 let cotacao = document.getElementById("cotacao")
 
+function getResult ( dataCotacao ){
+    return fetch ('https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='`${dataCotacao}`'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda').then(response => response.json())    
+}
+
 submit.addEventListener("click", () => {
-    let dia1 = new Date(document.getElementById("diaAtual").value)
-    let dia2 = new Date(document.getElementById("diaFinal").value)
-    console.log(dia1.toLocaleString(), dia2.toLocaleString())
+
+
+    let dia1 =(document.getElementById("diaAtual").value)
+    let dia2 =(document.getElementById("diaFinal").value)
+
+    const date = new Date(dia2).toLocaleDateString("en-US");
+    getResult(date)
+    
+    console.log(date)
+
 })
